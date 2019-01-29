813d39c5cdbe60723579830223b881135c8ad287
october <yanw.87@163.com>
2019-01-29 16:30:43 +0800

    管理输出

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+      app:'./src/index.js',
+      print:'./src/print.js'
+  },
+  plugins:[//插件管理输出
+    new CleanWebpackPlugin(['dist']),//清理 /dist 文件夹
+    new HtmlWebpackPlugin({          //新生成 index.html
+      title:'Output Management'
+    })
+  ],
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  module: {
-    rules: [
-            {
-            test: /\.css$/,
-            use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test:/\.(png|svg|jpg|gif)$/,
-                use:[
-                    'file-loader'
-                ]
-            },
-            {
-                test:/\.(woff|woff2|eot|ttf|otf)$/,
-                use:[
-                    'file-loader'
-                ]
-            },
-            {
-                test: /\.(csv|tsv)$/,
-                use: [
-                'csv-loader'
-                ]
-            },
-            {
-                test: /\.xml$/,
-                use: [
-                'xml-loader'
-                ]
-            }
-        ]
-    }
 };
